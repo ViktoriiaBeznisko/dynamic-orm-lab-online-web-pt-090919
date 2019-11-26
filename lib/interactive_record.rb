cb478c59bb00a71a36bfaecca59e6c5afacfc4b1
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-26 00:01:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -25,6 +25,12 @@ class InteractiveRecord
       self.send("#{name}=", value)
     end
   end
+  
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
 
   
 end
